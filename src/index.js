6e3264a1f646f10b2888cbf4c07d6187fd19141f
Ilyosbek0088 <ilyosbek0088@gmail.com>
2025-02-24 22:26:49 +0500

    ilya commit leg

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+AOS.init({
+  once: true, // Анимация сработает только один раз
+  duration: 1000, // Длительность анимации в миллисекундах
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-AOS.init();
